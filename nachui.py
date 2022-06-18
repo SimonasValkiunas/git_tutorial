24dd5b15ed00e6f095f9c13b158200d0de7b70e7
erambit <eviliute@gmail.com>
2022-06-18 23:18:41 +0300

    hello again

---
+++ nachui.py
@@ -0,0 +1,23 @@
+tired = True
+ready_to_hop = False
+
+if not tired and ready_to_hop:
+    print("Hopped")
+else:
+    print("I will not hop")
+
+tired = False
+ready_to_hop = True
+
+if not tired and ready_to_hop:
+    print("Hopped")
+else:
+    print("I will not hop")
+
+tired_two = False
+ready_to_hop_two = True
+
+if not tired_two and ready_to_hop_two:
+    print("Hopped")
+else:
+    print("I will not hop")
